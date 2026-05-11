9df3303e011add71d3281230cc7e08e0f413aceb
Mohammad Fathian <167574608+mmdfathian@users.noreply.github.com>
2026-05-11 20:12:29 +0330

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel, HttpUrl
+from datetime import datetime
+
+
+class URLCreate(BaseModel):
+    url: HttpUrl
+
+
+class URLInfo(BaseModel):
+    id: int
+    original_url: str
+    short_code: str
+    clicks: int
+    created_at: datetime
+    short_url: str
+
+    class Config:
+        from_attributes = True
 
