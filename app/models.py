15ce3f808496886d57129978a070f8f3bbfe7c97
Mohammad Fathian <167574608+mmdfathian@users.noreply.github.com>
2026-05-11 20:11:57 +0330

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from .database import Base
+
+
+class URLMap(Base):
+    __tablename__ = "url_maps"
+
+    id = Column(Integer, primary_key=True, index=True)
+    original_url = Column(String, nullable=False)
+    short_code = Column(String, unique=True, index=True, nullable=False)
+    clicks = Column(Integer, default=0)
+    created_at = Column(DateTime, default=datetime.utcnow)
 
