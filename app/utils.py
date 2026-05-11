70da0ad8a01c81d687552c35f43d7d459f692fc3
Mohammad Fathian <167574608+mmdfathian@users.noreply.github.com>
2026-05-11 20:13:01 +0330

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,8 @@
+import random
+import string
+
+
+def generate_short_code(length: int = 6) -> str:
+    chars = string.ascii_letters + string.digits
+    return "".join(random.choice(chars) for _ in range(length))
 
